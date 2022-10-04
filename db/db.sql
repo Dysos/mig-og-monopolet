dc9a4604fdff1f332b84b1247fddfd75ed5714ec
Dysos <victorj94@aol.com>
2022-10-04 10:59:55 +0200

    added table to db

---
+++ db/db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE dilemmas (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(5000),
+    answer1 VARCHAR(250) DEFAULT '',
+    answer2 VARCHAR(250) DEFAULT '',
+    answer3 VARCHAR(250) DEFAULT '',
+    answer4 VARCHAR(250) DEFAULT '',
+    answer1count INT DEFAULT 0,
+    answer2count INT DEFAULT 0,
+    answer3count INT DEFAULT 0,
+    answer4count INT DEFAULT 0
+);
+
+
+
+INSERT INTO dilemmas(question, answer1, answer2) VALUES ('Letmælk eller sødmælk?', 'Letmælk', 'Sødmælk');
+
+
